bccbab0d6b44a3e63f02360f0684561dbff8d84d
lennonalmeida <lennonraribeiro@gmail.com>
2018-07-07 14:18:41 -0300

    relay test

--- controller/home_auto.py
+++ controller/home_auto.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-
+import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(2,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)
+GPIO.setup(4,GPIO.OUT)
 
 import mysql.connector
 from datetime import date, datetime, timedelta
 from mysql.connector import errorcode
 
-def read_db():
-    #read state of db
-    a = ''
-    #a[0] = id from db
-    #a[1] = state from db
-    return a
+# def read_db():
+#     #READ db
+#     comand_show = 'SELECT state FROM app2state WHERE state BETWEEN 1 AND 2;'
+#     cursor.execute(comand_show)
+#     out = cursor.fetchall()
+#
+#     for i in out:
+#         print(i)
 
 def update_ap(id_gpio, state):
-    #update appliance
-    GPIO.output(id_gpio, state)
+     #update appliance
+     GPIO.output(id_gpio, state)
 
-def update_db(cursor, data_db):
-    #update db if diff_db == True
-    update_state = ("UPDATE %s SET STATE %s WHERE ID = %s;")
-    cursor.execute(update_state, data_db)
-    cursor.commit()
-
-def add_app:
-    #add a appliance
-    #stand by
-
-def diff_db(new):
-    #return True if db != appliance
-    #TODO compare db with 'new'
-    status = False
-    return status
-
+# def diff_db(new):
+#     #return True if db != appliance
+#     #TODO compare db with 'new'
+#     status = False
+#     return status
+#
 #main code
-
-data_db ={
-    db_name = 'pi',
+if __name__ == '__main__':
+    db_name = 'pi'
     table_name = 'app2state'
-    id = '0',
-}
+    #TODO id to all appliance
 
-db = mysql.connector.connect(user="root", password="mysql", database=db_name)
-cursor = db.cursor()
+    #connect to db
+    db = mysql.connector.connect(user="root", password="mysql", database=db_name)
+    cursor = db.cursor()
 
-while True:
-    new = read_db()
-    if diff_db(new):
-        update_ap(new)
+    #read_db()
+
+    #update db
+    while True:
+        if state == 'GPIO.HIGH':
+            state = 'GPIO.OUT'
+        else:
+            state = 'GPIO.HIGH'
+            update_ap(2, state)
+        time.sleep(2)
+    db.close()
