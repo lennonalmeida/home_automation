2a6849ad01b5edb20a32484c94b31fcc6cb7c437
lennonalmeida <lennonraribeiro@gmail.com>
2018-07-07 19:53:36 +0000

    Test useful

--- controller/web_service.py
+++ controller/web_service.py
@@ -1,18 +1,36 @@
 import mysql.connector
 from bottle import route, run
 
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(2, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
+
+
 @route('/hello')
 def hello():
     return "Hello World!"
-
-@route('/insert/<state>')
-def insert(state):
-    cnx = mysql.connector.connect(user='root', password='mysql', database='pi')
-    add_state = ("INSERT INTO app2state (state) VALUES (%(state)s)")
-    data_state = {'state': state}
+@route('/insert/<state>/<id_pi>')
+def insert(state, id_pi):
+    print(state, id_pi)
+    cnx = mysql.connector.connect(user='pidb', password='mysql', database='pi')
+    add_state = ("UPDATE app2state SET  state = '"+state+"' WHERE id = "+id_pi+";")
+    data_db = (state, id_pi)
+    #data_state = {'state': state}
     cursor = cnx.cursor()
-    cursor.execute(add_state, data_state)
+    cursor.execute(add_state)
     cnx.commit()
     cursor.close()
     cnx.close()
-run(host='192.168.0.8', port=8080, debug=True)
+    id_pi = int(id_pi)
+    update_ap((id_pi+1), state)
+def update_ap(id_gpio, state):
+    if state == 'OFF':
+        state = 1
+    else:
+        state = 0
+    GPIO.output(id_gpio, state)
+run(host='192.168.0.11', port=8080, debug=True)
+
